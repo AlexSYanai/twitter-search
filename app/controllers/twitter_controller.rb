6687992ac97ceb25997df6a4413f1fdd83769c3d
AlexSYanai <yanai.alex@gmail.com>
2015-05-16 13:52:52 -0400

    makes minor changes to twitter controller

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -24,26 +24,26 @@ class TwitterController < ActionController::Base
   private
   def parse_user(user)
     info = {
-      username: user.screen_name.to_s,
-      full_name: user.name.to_s,
-      tweets: user.statuses_count.to_s,
-      favorites: user.favorites_count.to_s,
-      followers: user.followers_count.to_s,
-      friends: user.friends_count.to_s,
+      username:    user.screen_name.to_s,
+      full_name:   user.name.to_s,
+      tweets:      user.statuses_count.to_s,
+      favorites:   user.favorites_count.to_s,
+      followers:   user.followers_count.to_s,
+      friends:     user.friends_count.to_s,
       description: user.description.to_s,
-      prof_pic: user.profile_image_url_https.to_s,
-      location: user.location.to_s,
-      user_score:( @tweet_sentiment + user.followers_count).to_s,
-      background: user.profile_background_image_url_https.to_s }
-    end
+      location:    user.location.to_s,
+      prof_pic:    user.profile_image_url_https.to_s,
+      background:  user.profile_background_image_url_https.to_s,
+      user_score:  (@tweet_sentiment + user.followers_count).to_s }
+  end
 
-    def parse_tweets(tweets)
-      tweets_info = tweets.map do |tweet|
-        { date: format_date(tweet.created_at.to_s),
-          retweets: tweet.retweet_count.to_s,
-          body: tweet.text.to_s,
-          sentiment: analyze_sentiment(tweet.text) }
-      end
+  def parse_tweets(tweets)
+    tweets_info = tweets.map do |tweet|
+      { date:      format_date(tweet.created_at.to_s),
+        retweets:  tweet.retweet_count.to_s,
+        body:      tweet.text.to_s,
+        sentiment: analyze_sentiment(tweet.text) }
+    end
     @tweet_sentiment = tweets_info.map { |n| n[:sentiment].to_i }.inject(:+)
     tweets_info
   end
