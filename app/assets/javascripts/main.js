ac6486979ef6c16893d447ad8d7c249fbebbad69
AlexSYanai <yanai.alex@gmail.com>
2015-05-17 09:21:34 -0400

    implements sorting for all 3 categories

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,19 +3,28 @@ $(document).ready(function(){
     var key = e.which;
     var newUser = { name : $('#user-search').val() }
     if (key == 13) {
-      $.ajax({
-        type: 'POST',
-        url: '/twitter',
-        data: newUser,
-        dataType: 'json',
-        success: function(data) {
-          var tweetArray = data.info[0]
-          var currentUser = data.info[1]
-          updateProfile(currentUser,tweetArray)
-        }
-      });
+      retrieveProfile(newUser)
     }
   });
+
+  $('#search-button').click(function(){
+    var newUser = { name : $('#user-search').val() }
+    retrieveProfile(newUser)
+  });
+
+  function retrieveProfile(newUser) {
+    $.ajax({
+      type: 'POST',
+      url: '/twitter',
+      data: newUser,
+      dataType: 'json',
+      success: function(data) {
+        var tweetArray = data.info[0]
+        var currentUser = data.info[1]
+        updateProfile(currentUser,tweetArray)
+      }
+    });
+  }
 
   function updateProfile(user,tweets) {
     for (prop in user) {
@@ -38,60 +47,145 @@ $(document).ready(function(){
     }
 
     $.each(tweets, function(key,value) {
-      var tweetDiv = $(".tweets" + (key + 1))
-      $.each(tweetDiv[0].children, function(prop,val) {
+      var $tweetDiv = $("#tweet" + (key + 1))
+      $.each($tweetDiv[0].children, function(prop,val) {
         tweetVal = val.className
         if (tweetVal !== "username") {
-          val.innerHTML = value[tweetVal];
+          if (tweetVal === "tweet_pic") {
+            if (value[tweetVal] !== null) {
+              var $imgDiv = val.children[0]
+              $(val).attr('value','1')
+              $($imgDiv).attr('src', "http://" + value[tweetVal].host + value[tweetVal].path)
+            }
+          }else{
+            val.innerHTML = value[tweetVal];
+          }
         }
       });
     });
   }
 
-
-  $.sortByDate = function( elements, order ) {
-    console.log('hisort');
+  function sortByDate(elements, order) {
     var arr = [];
     elements.each(function() {
       var obj = {},
       $els = $(this),
       $el = $els[0]
       $created_div = $($el.children)
-      time = $created_div[4].innerHTML,
+      time = $created_div[5].innerHTML,
       date = new Date(time),
       timestamp = date.getTime();
-      obj.html = $created_div[4].parentElement;
-      obj.time = timestamp;      
+      obj.html = $created_div[5].parentElement;
+      obj.time = timestamp;
+
       arr.push( obj );
     });
 
-    var sorted = arr.sort(function( a, b ) {
-      if( order == "ASC" ) {
+    var sorted = arr.sort(function(a, b) {
+      if( order == 'ASC' ) {
         return a.time > b.time;
       } else {
         return b.time > a.time;
       }
+
+    });
+    return sorted;
+  };
+
+  function sortByRetweets(elements, order) {
+    var arr = [];
+    elements.each(function() {
+      var obj = {},
+      $els = $(this),
+      $el = $els[0]
+      $retweetDiv = $($el.children)
+      $retweetCount = $retweetDiv[2].innerHTML,
+      rtNum = parseInt($retweetCount),
+      obj.html = $retweetDiv[2].parentElement;
+      obj.retweets = rtNum;
+
+      arr.push( obj );
     });
 
+    var sorted = arr.sort(function(a, b) {
+      if( order == 'ASC' ) {
+        return a.retweets > b.retweets;
+      } else {
+        return b.retweets > a.retweets;
+      }
+
+    });
+    return sorted;
+  };
+
+  function sortByPic(elements, order) {
+    var arr = [];
+    elements.each(function() {
+      var obj = {},
+      $els = $(this),
+      $el = $els[0].children[4];
+      $picVal = $($el).attr('value')
+      hasPic = parseInt($picVal),
+      obj.html = $el.parentElement;
+      obj.pic = hasPic;
+
+      arr.push( obj );
+    });
+
+    var sorted = arr.sort(function(a, b) {
+      if( order == 'ASC' ) {
+        return a.pic > b.pic;
+      } else {
+        return b.pic > a.pic;
+      }
+
+    });
     return sorted;
   };
 
   $(function() {
-    var $newer = $( "#newer" ),
-    $older = $( "#older" ),
-    $content = $( "#sliderList" ),
-    $elements = $( ".tweet" );
+    var $content = $( "#sliderList" );
+    var $elements = $( ".tweet" );
 
-    $older.click(function() {
-      var elements = $.sortByDate( $elements, "ASC" );
+    $('#created').click(function() {
+      $creationOrder = $( '#created' ).attr('value');
+      if ($creationOrder == "newer"){
+        var elements = sortByDate($elements, 'ASC');
+        $( '#created' ).attr('value','older');
+      }else{
+        var elements = sortByDate($elements, 'DESC');
+        $( '#created' ).attr('value','newer');
+      }
       for( var i = 0; i < elements.length; ++i ) {
         $('#sliderList').append($(elements[i].html))
       }
       return false;
     });
 
-    $newer.click(function() {
-      var elements = $.sortByDate( $elements, "DESC" );
+    $('#rts').click(function() {
+      $retweetOrder = $( '#rts' ).attr('value');
+      if ($retweetOrder == 'highest'){
+        var elements = sortByRetweets($elements, 'ASC');
+        $('#rts').attr('value','lowest');
+      }else{
+        var elements = sortByRetweets($elements, 'DESC');
+        $('#rts').attr('value','highest');
+      }
+      for( var i = 0; i < elements.length; ++i ) {
+        $('#sliderList').append($(elements[i].html))
+      }
+      return false;
+    });
+
+    $('#anyPics').click(function() {
+      $picOrder = $( '#anyPics' ).attr('value');
+      if ($picOrder == 'yes'){
+        var elements = sortByPic($elements, 'ASC');
+        $('#anyPics').attr('value','no');
+      }else{
+        var elements = sortByPic($elements, 'DESC');
+        $('#anyPics').attr('value','yes');
+      }
       for( var i = 0; i < elements.length; ++i ) {
         $('#sliderList').append($(elements[i].html))
       }
